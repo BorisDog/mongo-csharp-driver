4cf20cdd98a39a8b4aec8be2d8122c164239fc36
Boris <boris.dogadov@mongodb.com>
2021-08-10 17:20:05 -0700

    - PR comments

--- src/MongoDB.Driver.Core/Core/Operations/BulkMixedWriteOperation.cs
+++ src/MongoDB.Driver.Core/Core/Operations/BulkMixedWriteOperation.cs
@@ -215,14 +215,13 @@ namespace MongoDB.Driver.Core.Operations
             {
                 EnsureCollationIsSupportedIfAnyRequestHasCollation(context);
                 EnsureHintIsSupportedIfAnyRequestHasHint(context);
-
                 context.DisableRetriesIfAnyWriteRequestIsNotRetryable(_requests);
                 var helper = new BatchHelper(_requests, _isOrdered, _writeConcern);
                 foreach (var batch in helper.GetBatches())
                 {
                     batch.Result = ExecuteBatch(context, batch, cancellationToken);
                 }
-                return helper.GetFinalResultOrThrow(context.Channel?.ConnectionDescription?.ConnectionId);
+                return helper.GetFinalResultOrThrow(context.Channel.ConnectionDescription?.ConnectionId);
             }
         }
 
@@ -234,14 +233,13 @@ namespace MongoDB.Driver.Core.Operations
             {
                 EnsureCollationIsSupportedIfAnyRequestHasCollation(context);
                 EnsureHintIsSupportedIfAnyRequestHasHint(context);
-
                 context.DisableRetriesIfAnyWriteRequestIsNotRetryable(_requests);
                 var helper = new BatchHelper(_requests, _isOrdered, _writeConcern);
                 foreach (var batch in helper.GetBatches())
                 {
                     batch.Result = await ExecuteBatchAsync(context, batch, cancellationToken).ConfigureAwait(false);
                 }
-                return helper.GetFinalResultOrThrow(context.Channel?.ConnectionDescription?.ConnectionId);
+                return helper.GetFinalResultOrThrow(context.Channel.ConnectionDescription?.ConnectionId);
             }
         }
 
