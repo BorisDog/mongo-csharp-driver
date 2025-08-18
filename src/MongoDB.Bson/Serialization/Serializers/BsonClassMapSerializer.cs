0ea2d9a58a698b9783ba192ab621c3ecc0af044d
BorisDog <boris.dogad@gmail.com>
2025-08-18 13:08:54 -0700

    - Temp 2

--- src/MongoDB.Bson/Serialization/Serializers/BsonClassMapSerializer.cs
+++ src/MongoDB.Bson/Serialization/Serializers/BsonClassMapSerializer.cs
@@ -145,7 +145,7 @@ namespace MongoDB.Bson.Serialization
                     supportsInitialization.BeginInit();
                 }
             }
-            // Comment to delete 123
+            // Comment to delete 123123
             var discriminatorConvention = _classMap.GetDiscriminatorConvention();
             var allMemberMaps = _classMap.AllMemberMaps;
             var extraElementsMemberMapIndex = _classMap.ExtraElementsMemberMapIndex;
